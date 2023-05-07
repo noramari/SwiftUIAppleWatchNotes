b5a16332ec754245339d507c13a8584d6b901f13
noramari <noora@selinan.com>
2023-05-07 18:16:44 +0900

    Initial Commit

---
+++ SwiftUIAppleWatchNotes Watch App/SwiftUIAppleWatchNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppleWatchNotesApp.swift
+//  SwiftUIAppleWatchNotes Watch App
+//
+//  Created by Noora Maeda on 2023/05/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppleWatchNotes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
