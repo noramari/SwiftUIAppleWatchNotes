40ac0b312769ad4bbc60cc952fe6d7af740fc423
noramari <noora@selinan.com>
2023-05-08 15:01:48 +0900

    Add functionality to save, load and delete notes

--- SwiftUIAppleWatchNotes Watch App/View/ContentView.swift
+++ SwiftUIAppleWatchNotes Watch App/View/ContentView.swift
@@ -13,35 +13,95 @@ struct ContentView: View {
     @State private var text: String = ""
     
     var body: some View {
-        VStack {
-            HStack(alignment: .center, spacing: 6) {
-                TextField("Add New Note", text: $text)
-                
-                Button {
-                    guard text.isEmpty == false else { return }
-                    let note = Note(id: UUID(), text: text)
-                    notes.append(note)
-                    text = ""
-                    save()
-                } label: {
-                    Image(systemName: "plus.circle")
-                        .font(.system(size: 42, weight: .semibold))
+        NavigationView {
+            VStack {
+                HStack(alignment: .center, spacing: 6) {
+                    TextField("Add New Note", text: $text)
+                    
+                    Button {
+                        guard text.isEmpty == false else { return }
+                        let note = Note(id: UUID(), text: text)
+                        notes.append(note)
+                        text = ""
+                        save()
+                    } label: {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 42, weight: .semibold))
+                    }
+                    .fixedSize()
+                    .buttonStyle(PlainButtonStyle())
+                    .foregroundColor(.accentColor)
                 }
-                .fixedSize()
-                .buttonStyle(PlainButtonStyle())
-                .foregroundColor(.accentColor)
+                
+                Spacer()
+                
+                if notes.count >= 1 {
+                    List {
+                        ForEach(0..<notes.count, id: \.self) { i in
+                            HStack {
+                                Capsule()
+                                    .frame(width: 4)
+                                    .foregroundColor(.accentColor)
+                                Text(notes[i].text)
+                                    .lineLimit(1)
+                                    .padding(.leading, 5)
+                            }
+                        }
+                        .onDelete(perform: delete)
+                    }
+                } else {
+                    Spacer()
+                    Image(systemName: "note.text")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.gray)
+                        .opacity(0.25)
+                        .padding(25)
+                    Spacer()
+                }
+                
             }
-            
-            Spacer()
-            
-            Text("\(notes.count)")
+            .navigationTitle("Notes")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear() {
+                load()
+            }
         }
-        .navigationTitle("Notes")
-        .padding()
+    }
+    
+    //MARK: - Functions
+    func getDocumentDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
     }
     
     func save() {
-        dump(notes)
+        do {
+            let data = try JSONEncoder().encode(notes)
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+            try data.write(to: url)
+        } catch {
+            print("Saving data has failed.")
+        }
+    }
+    
+    func load() {
+        DispatchQueue.main.async {
+            do {
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                let data = try Data(contentsOf: url)
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch {
+                
+            }
+        }
+    }
+    
+    func delete(offsets: IndexSet) {
+        withAnimation {
+            notes.remove(atOffsets: offsets)
+            save()
+        }
     }
 }
 
